452de0638046c594949dc90c6f0abeba576bf2ce
pro_dip <prodip.sarker@sslwireless.com>
2024-11-14 10:44:46 +0600

    added type orm and fix eslint issue

--- service-app/src/app.module.ts
+++ service-app/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './product/products.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ service-app/src/product/products.service.ts
@@ -0,0 +1,42 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Product } from './product.entity';
+
+@Injectable()
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+  ) {}
+
+  async findAll(): Promise<Product[]> {
+    return this.productsRepository.find();
+  }
+
+  async findOne(id: number): Promise<Product> {
+    const product = await this.productsRepository.findOneBy({
+      id,
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with ID ${id} not found`);
+    }
+    return product;
+  }
+
+  async create(productData: Partial<Product>): Promise<Product> {
+    const product = this.productsRepository.create(productData);
+    return this.productsRepository.save(product);
+  }
+
+  async update(id: number, productData: Partial<Product>): Promise<Product> {
+    await this.findOne(id); // Check if product exists
+    await this.productsRepository.update(id, productData);
+    return this.findOne(id);
+  }
+
+  async remove(id: number): Promise<void> {
+    const product = await this.findOne(id);
+    await this.productsRepository.remove(product);
+  }
+}
